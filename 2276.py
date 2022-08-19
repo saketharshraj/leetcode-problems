21763cd9a1d825d6b9dfa1b42b8b34bdf01ba950
Harsh Raj <harsh.smartters@gmail.com>
2022-08-19 20:55:51 +0530

    Covered few more corner cases

--- 2276.py
+++ 2276.py
@@ -8,30 +8,36 @@ class CountIntervals:
 
     def add(self, left: int, right: int) -> None:
         old_range, new_range = None, None
+
         for i in range(len(self.data)):
             if new_range:
                 break
             interval = self.data[i]
             lower, upper = interval[0], interval[1]
             old_range = [lower, upper]
-            if left < lower and right > upper:
+
+            if right < lower or left > upper:
+                break
+            elif left >= lower and right <= upper:
+                return None  # no need change anything in data
+
+            elif left <= lower and right >= upper:
                 self.data[i][0] = left
                 self.data[i][1] = right
                 new_range = [left, right]
+
             elif left < lower and lower <= right <= upper:
                 self.data[i][0] = left
                 new_range = [left, upper]
             elif lower <= left <= upper and right > upper:
                 self.data[i][1] = right
                 new_range = [lower, right]
-            elif left >= lower and right <= upper:
-                new_range = 1  # no need change anything in data
+            
         if not new_range:
             self.data.append([left, right])
             self.rangeCount += right - left + 1
             return None
-        if new_range == 1:
-            return None
+
         old = old_range[1] - old_range[0] + 1
         new = new_range[1] - new_range[0] + 1
         self.rangeCount += new - old
